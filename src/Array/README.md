77eb6083e9db6b50322af15484df3f0370a65cb9
Akshay Kumar Maurya <akshay21sep@gmail.com>
2024-07-27 23:43:14 +0530

    Span of array

--- src/Array/README.md
+++ src/Array/README.md
@@ -1,2 +1,23 @@
-# DSA-Java
-# Array
+# DSA-Java - Array
+
+#### Span of Array
+![Span Of Array Image](../assets/array/spanofarrayque1.png)
+![Span Of Array Image](../assets/array/spanofarrayque2.png)
+
+#### [PC:L-93 => Que-93 & Sol-94]
+
+```
+Input Format    
+int n = 6
+'n' where n is any integer value.
+int[] arr = new int[]{15, 30, 40, 4, 11, 9};
+'arr' where arr is int array.
+
+# E.g.
+max = 40
+min = 4
+difference = 36
+
+o/p:
+result = 36
+```
---
+++ src/Array/SpanOfArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class SpanOfArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{15, 30, 40, 4, 11, 9};
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        int difference = max - min;
+        System.out.println("Difference value - " + difference);
+    }
+}
