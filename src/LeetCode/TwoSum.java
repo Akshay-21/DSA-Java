a50aaf330f2a44fec1659e11642f3feaad15c91f
Akshay Kumar Maurya <akshay21sep@gmail.com>
2024-08-10 18:48:19 +0530

    Two Sum

---
+++ src/LeetCode/TwoSum.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+        int[] num = {2, 7, 11, 15};
+        int target = 9;
+        int[] indexValue = getArrayIndexes(num, target);
+
+        for (int i = 0; i < indexValue.length; i++) {
+            System.out.println(indexValue[i]);
+        }
+    }
+
+    @NotNull
+    private static int[] getArrayIndexes(int[] num, int target) {
+        int[] indexValue = new int[2];
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] + num[j] == target) {
+                    indexValue[0] = i;
+                    indexValue[1] = j;
+                }
+            }
+        }
+        return indexValue;
+    }
+}
+
+/**
+ *  The program complexity is O(n2).
+ * */
